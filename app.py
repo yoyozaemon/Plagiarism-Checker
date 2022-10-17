f1827ed7d5165cd9a1286a73e596bfb369d97016
Vijay J <81014244+yoyozaemon@users.noreply.github.com>
2022-10-17 09:19:49 +0530

    updated

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-import os from sklearn.feature_extraction.text
-import TfidfVectorizer from sklearn.metrics.pairwise
-import cosine_similariity
+import os
+import re
+from sklearn.feature_extraction.text import TfidfVectorizer 
+from sklearn.metrics.pairwise import cosine_similariity
 
 student_files = [doc for doc in os.listdir() if doc.endswith('.txt')]
-student_notes = [open(_file ,encoding='utf-8').read()]
+student_notes = [open(__file ,encoding='utf-8').read() for_file in student_files]
+
+def vectorize(Text):
+    return TfidfVectorizer().fit_transform(Text).toarray()
+def similarity(doc1,doc2):
+    return  cosine_similariity([doc1,doc2])
+
+    
